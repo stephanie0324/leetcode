3ec3345fedf063ae397eb262ce577c65be67b604
stephanie0324 <40812696+stephanie0324@users.noreply.github.com>
2024-04-23 18:01:23 +0800

    Time: 64 ms (75.00%), Space: 12.9 MB (44.42%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        st = []
+        for x in asteroids:
+            surv = True
+            if x > 0:
+                st.append(x)
+            else:
+                while len(st) > 0:
+                    y = st[-1]
+                    if y < 0:
+                        break
+                    elif x + y == 0:
+                        surv = False
+                        st.pop()
+                        break
+                    elif x + y < 0:
+                        st.pop()
+                    else:
+                        surv = False
+                        break
+                if surv:
+                    st.append(x)
+        return st
+        
